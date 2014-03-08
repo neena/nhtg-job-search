00f5f8ae0c45a7258407889fb440f6e85950c492
Neena <junkemail273@gmail.com>
2014-03-08 13:00:17 +0000

    Rails rules the world.

---
+++ job-site/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobSite::Application.config.secret_key_base = '1918941119be8b71776c8415c775cb8cf23c61a8393d4bd96b057507813eb4679aa0fa3ea2f69ce9e2354b50302520c9c7a4f5a23581e9ee78e18b6c18e54f9d'
