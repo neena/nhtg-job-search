aae9164bb4ef5b466ab75a3d3954d6cac476d117
Andrew Deniszczyc <andy.deniszczyc@gmail.com>
2014-03-08 19:56:20 +0000

    Added geolocation autofill

--- job-site/app/assets/javascripts/geolocation.js
+++ job-site/app/assets/javascripts/geolocation.js
@@ -1,24 +1,62 @@
-function find_city(element) {
-	if (navigator.geolocation) {
-     	navigator.geolocation.getCurrentPosition(function(position) {
-     		var lat = position.coords.latitude;
-   			var lng = position.coords.longitude;
-   			var url = "http://uk-postcodes.com/latlng/" + lat + "," + lng + ".json?callback=alertme";
-   			console.log(url);
-   			$.ajax({
-			    url: url,
-			    dataType: "jsonp",
-			    jsonp: true,
-			    type: 'GET',
-			    crossDomain: true,
-			    success: function(response) {
-			        console.log(response.postcode); // server response
-			    }
-			});
-     	}, null);
-	} 
-}
-function alertme(data) {
-	console.log(data);
-}
-find_city();
+var geolocation = (function() {
+     var self = {
+
+      checkGeolocation: function() {
+        if (navigator.geolocation) {
+          return true;
+        }
+        return false;
+      },
+
+      getLocation: function() {
+        var promise = $.Deferred();
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {};
+          pos.lat = position.coords.latitude;
+          pos.lng = position.coords.longitude;
+          promise.resolve(pos);
+        });
+        return promise;
+      },
+
+      ajax: function(url) {
+        return $.ajax(url, {
+	        crossDomain:true, 
+			    dataType: "jsonp", 		     
+		    });
+      },
+
+      nearPostcode: function(element) {
+        var promise = $.Deferred();
+        if (this.checkGeolocation()) {	
+  	      var getLocation = self.getLocation();
+          getLocation.then(function(pos) {
+            var url = "http://api.postcodes.io/postcodes/lon/" + pos.lng + "/lat/" + pos.lat;
+            var getPostcode = self.ajax(url);
+            getPostcode.success(function(data) {
+              var name = data.result[0].admin_ward;
+              promise.resolve(name);
+            });
+
+          }); 
+        }
+        return promise;
+      },
+
+      findElements: function() {
+        // Find textboxes looking for geolocation
+        var geolocation_city = $('.js-geolocation-city').length;
+        if (geolocation_city > 0) {
+           var city = self.nearPostcode();
+           city.then(function(name) {
+            $('.js-geolocation-city').val(name);
+           });
+        }
+      }
+    };
+    return self;
+}());
+
+$(function() {
+  geolocation.findElements();
+});  
