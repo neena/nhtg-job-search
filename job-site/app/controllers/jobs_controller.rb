d682d1cab7b64bb0d1fad0a0f4d508abc956353e
Neena <junkemail273@gmail.com>
2014-03-08 15:37:16 +0000

    JSON requests and loading from static file.

--- job-site/app/controllers/jobs_controller.rb
+++ job-site/app/controllers/jobs_controller.rb
@@ -1,8 +1,20 @@
 class JobsController < ApplicationController
+	require 'open-uri'
+	require 'json'
+
 	def index
 		# Search form. 
 	end
 	def search
+		@jobs = JSON.parse(open(Rails.root + "public/search.json").read)
+		# JSON.parse(open("http://yagajobs.co.uk/api/vacancies.json/search?#{parameterize}").read)
+	end
 
+	private 
+	def parameterize 
+		params.delete("utf8")
+		params.delete("commit")
+		params["api_key"] = "9ef095b57dc3e3eb7449bfcffbc01b54"
+		params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
 	end
 end
