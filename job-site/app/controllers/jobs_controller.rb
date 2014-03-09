2758fa1fcdf0df728962f9b10785c237a95f1b66
Neena <junkemail273@gmail.com>
2014-03-09 14:24:08 +0000

    Don't hate me but I cba to write a commit message.
    Commutable locations work, I think.

--- job-site/app/controllers/jobs_controller.rb
+++ job-site/app/controllers/jobs_controller.rb
@@ -8,19 +8,16 @@ class JobsController < ApplicationController
 
 	def search
 		# @jobs = JSON.parse(open("http://yagajobs.co.uk/api/vacancies.json/search?#{parameterize(params)}").read)
+		get_places_near params["home"]
+
+		params.delete("utf8")
+		params.delete("commit")
+		params["location"] = params["home"] unless params["location"]
+		params["api_key"] = "9ef095b57dc3e3eb7449bfcffbc01b54"
+
 		@jobs = JSON.parse(open(Rails.root+"public/search.json").read)
 		@jobs.each do |job|
-			if job["salaryMinYearly"].try{ |s| s > 11000}
-				job["salaryMinMonthly"] = job["salaryMinYearly"] / 12
-				job["salaryMaxMonthly"] = job["salaryMaxYearly"] / 12
-			else
-				job["salaryMinHourly"] = job["salaryMinYearly"]
-				job["salaryMaxHourly"] = job["salaryMaxYearly"]
-				if job["hours"].try{|h| h.downcase.include? "full"}
-					job["salaryMinMonthly"] = job["salaryMinHourly"].to_i*(37*365/7)/12
-					job["salaryMaxMonthly"] = job["salaryMaxHourly"].to_i*(37*365/7)/12
-				end
-			end
+			correct_salary job
 		end
 
 		respond_to do |format|
@@ -38,11 +35,27 @@ class JobsController < ApplicationController
 
 	private 
 	def parameterize params
-		params.delete("utf8")
-		params.delete("commit")
-		params["location"] = params["home"] unless params["location"]
-		params.delete("home")
-		params["api_key"] = "9ef095b57dc3e3eb7449bfcffbc01b54"
 		params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')
+	end
+
+	def get_places_near place
+		box = Geocoder::Calculations.bounding_box(Geocoder.coordinates(place), 10)
+		params = {username: "jubjubbird01", north: box[2], south: box[0], east: box[3], west: box[1]}
+		@places = JSON.parse(open("http://api.geonames.org/citiesJSON?#{parameterize(params)}").read)
+		p @places
+	end
+
+	def correct_salary job
+		if job["salaryMinYearly"].try{ |s| s > 11000}
+			job["salaryMinMonthly"] = job["salaryMinYearly"] / 12
+			job["salaryMaxMonthly"] = job["salaryMaxYearly"] / 12
+		else
+			job["salaryMinHourly"] = job["salaryMinYearly"]
+			job["salaryMaxHourly"] = job["salaryMaxYearly"]
+			if job["hours"].try{|h| h.downcase.include? "full"}
+				job["salaryMinMonthly"] = job["salaryMinHourly"].to_i*(37*365/7)/12
+				job["salaryMaxMonthly"] = job["salaryMaxHourly"].to_i*(37*365/7)/12
+			end
+		end
 	end
 end
